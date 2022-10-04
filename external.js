ae61c573306ce8400ea432e43dc11fafcfc9f65c
Rushikesh <rushichindage@gmail.com>
2022-10-04 03:13:42 -0700

    commit arethnatic

---
+++ external.js
@@ -0,0 +1,9 @@
+let x = 5;         // assign the value 5 to x
+let y = 2;         // assign the value 2 to y
+let z = x + y;
+console.log(z);
+let a = x*y;
+let b = x-y;
+let c = x%y;
+let d = x/y;
+console.log(a,b,c,d);
